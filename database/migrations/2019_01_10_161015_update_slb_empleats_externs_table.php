ca89884b79cb3a797280659148d2fe151d53dd34
Abraham <brugueracalvo@gmail.com>
2019-01-11 16:15:49 +0100

    Habrá que vigilar con los nombres de las clases de las migraciones.

--- database/migrations/2019_01_10_161015_update_slb_empleats_externs_table.php
+++ database/migrations/2019_01_10_161015_update_slb_empleats_externs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateSlbEmpleatsExternsTable extends Migration
+class Update2SlbEmpleatsExternsTable extends Migration
 {
     /**
      * Run the migrations.
