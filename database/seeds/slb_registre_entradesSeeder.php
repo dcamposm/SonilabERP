1b6ae2657b08786a82870e104f6f2b30751c8932
Kevin Gómez Codina <kevin.gomez.codina@gmail.com>
2019-01-31 19:45:09 +0100

    Autor: Kevin Gómez Codina Commit: - Se ha añadido las seeders para el apartado de registro de entradas.

---
+++ database/seeds/slb_registre_entradesSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\RegistreEntrada;
+
+class slb_registre_entradesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB:: table('slb_registre_entrades')->delete();
+        RegistreEntrada:: create(array(
+            'id_registre_entrada'         => 1,
+            'ot'        => '',
+            'oc' => '',
+            'titol'        => 'Registre de prova',
+            'id_client'        => 1,
+            'id_servei'        => 1,
+            'id_idioma'        => 1,
+            'id_media'        => 1,
+            'minuts'        => 69,
+            'total_episodis'        => 1,
+            'episodis_setmanals'        => 1,
+            'entregues_setmanals'        => 1,
+            'estat' => 'Pendent'
+        ));
+    }
+}
---
+++ database/seeds/slb_tipus_mediaSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\TipusMedia;
+
+class slb_tipus_mediaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB:: table('slb_tipus_media')->delete();
+        TipusMedia:: create(array(
+            'id_media'         => 1,
+            'nom_media'        => 'Documental',
+            'descripcio_media' => 'Documental.'
+        ));
+        TipusMedia:: create(array(
+            'id_media'         => 2,
+            'nom_media'        => 'Pel·lícula animació',
+            'descripcio_media' => 'Pel·lícula animada.'
+        ));
+        TipusMedia:: create(array(
+            'id_media'         => 3,
+            'nom_media'        => 'Pel·lícula Ficció A',
+            'descripcio_media' => 'Pel·lícula de ficció A.'
+        ));
+        TipusMedia:: create(array(
+            'id_media'         => 4,
+            'nom_media'        => 'Pel·lícula Ficció B',
+            'descripcio_media' => 'Pel·lícula de ficció B.'
+        ));
+        TipusMedia:: create(array(
+            'id_media'         => 5,
+            'nom_media'        => 'Serie Ficció',
+            'descripcio_media' => 'Serie de ficció.'
+        ));
+    }
+}
