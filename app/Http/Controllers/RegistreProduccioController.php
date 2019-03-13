60fd43bdac2b31f0496ddc69e1976ec064ff10bf
David Ortiz <cmortizdavid@gmail.com>
2019-03-13 18:20:04 +0100

    Creación registro de producción (primeros campos incompleto).

--- app/Http/Controllers/RegistreProduccioController.php
+++ app/Http/Controllers/RegistreProduccioController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\RegistreEntrada;
+use App\RegistreProduccio;
 
 class RegistreProduccioController extends Controller {
 
@@ -13,6 +15,14 @@ class RegistreProduccioController extends Controller {
     public function getIndex() {
         $registresProduccio = array('3838','001','29/09/2018','1','SHANE - THE DISTANT BELL');
         return View('registre_produccio.index', array('registreProduccio' => $registresProduccio));
+    }
+    
+    public function insertView(){
+        //Esto es temporal porque obviamente no mostraremos un desplegable con todos los registros de entrada.
+        $registreEntrada = RegistreEntrada::all();
+        $registreProduccio = RegistreProduccio::all();
+        
+        return View('registre_produccio.create', array('registresEntrada'=>$registreEntrada,'registreProducció'=>$registreProduccio));
     }
 
 }
