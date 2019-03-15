66dbdf4fa887683e9c9484d7b946e24046808532
Kevin Gómez Codina <kevin.gomez.codina@gmail.com>
2019-03-15 18:12:13 +0100

    Autor: Kevin Gómez Codina Commit: - Se han realizado todos los formularios de registros de producción en la vista de crear.

--- resources/views/registre_produccio/create.blade.php
+++ resources/views/registre_produccio/create.blade.php
@@ -110,17 +110,114 @@
               <input type="text" name="subtitol" id="subtitol" class="form-control">
             </div>
 
+            <div class="form-group col-12 col-sm-6">
+              <label for="qc_mix">QC MIX</label>
+              <select name="qc_mix" id="qc_mix" class="form-control">
+                <option value="false" selected>No</option>
+                <option value="true">Sí</option>
+              </select>
+            </div>
+
+            <div class="form-group col-12 col-sm-6">
+              <label for="ppe">PPE</label>
+              <select name="ppe" id="ppe" class="form-control">
+                <option value="false" selected>No</option>
+                <option value="true">Sí</option>
+              </select>
+            </div>
+
           </div>
         </form> {{-- end form --}}
       </div>
     </div>
 
-    {{-- MATERIAL AUDIOVISUAL --}}
+    {{-- EMPLEATS --}}
     <div class="col-12 col-xl-6 mb-3">
       <div class="card">
         <form action=""> {{-- init form --}}
           <div class="card-header">
-            <span class="h3">Material audiovisual</span>
+            <span class="h3">Empleats</span>
+            <button class="btn btn-success float-right">
+              <span class="fas fa-save"></span>Desar
+            </button>
+          </div>
+          <div class="card-body row">
+            <div class="form-group col-12 col-sm-6">
+              <label for="id_traductor">Traductor</label>
+              <select name="id_traductor" id="id_traductor" class="form-control">
+                @foreach ($empleats as $key => $empleat) 
+                  <option value="{{ $empleat->id_empleat }}">{{ $empleat->nom_empleat }} {{ $empleat->cognom1_empleat }} {{ $empleat->cognom2_empleat }}</option>
+                @endforeach
+              </select>
+            </div>
+
+            <div class="form-group col-12 col-sm-6">
+              <label for="data_traductor">Data traductor</label>
+              <input type="date" name="data_traductor" id="data_traductor" class="form-control">
+            </div>
+
+            <div class="form-group col-12 col-sm-6">
+              <label for="id_ajustador">Ajustador</label>
+              <select name="id_ajustador" id="id_ajustador" class="form-control">
+                @foreach ($empleats as $key => $empleat) 
+                  <option value="{{ $empleat->id_empleat }}">{{ $empleat->nom_empleat }} {{ $empleat->cognom1_empleat }} {{ $empleat->cognom2_empleat }}</option>
+                @endforeach
+              </select>
+            </div>
+
+            <div class="form-group col-12 col-sm-6">
+              <label for="data_ajustador">Data ajustador</label>
+              <input type="date" name="data_ajustador" id="data_ajustador" class="form-control">
+            </div>
+
+            <div class="form-group col-12 col-sm-6">
+              <label for="id_linguista">Lingüista</label>
+              <select name="id_linguista" id="id_linguista" class="form-control">
+                @foreach ($empleats as $key => $empleat) 
+                  <option value="{{ $empleat->id_empleat }}">{{ $empleat->nom_empleat }} {{ $empleat->cognom1_empleat }} {{ $empleat->cognom2_empleat }}</option>
+                @endforeach
+              </select>
+            </div>
+
+            <div class="form-group col-12 col-sm-6">
+              <label for="data_linguista">Data lingüista</label>
+              <input type="date" name="data_linguista" id="data_linguista" class="form-control">
+            </div>
+
+            <div class="form-group col-12 col-sm-6">
+              <label for="id_tecnic_mix">Tècnic mix</label>
+              <select name="id_tecnic_mix" id="id_tecnic_mix" class="form-control">
+                @foreach ($empleats as $key => $empleat) 
+                  <option value="{{ $empleat->id_empleat }}">{{ $empleat->nom_empleat }} {{ $empleat->cognom1_empleat }} {{ $empleat->cognom2_empleat }}</option>
+                @endforeach
+              </select>
+            </div>
+
+            <div class="form-group col-12 col-sm-6">
+              <label for="data_tecnic_mix">Data tècnic mix</label>
+              <input type="date" name="data_tecnic_mix" id="data_tecnic_mix" class="form-control">
+            </div>
+
+            <div class="form-group col-12 col-sm-6">
+              <label for="id_director">Director</label>
+              <select name="id_director" id="id_director" class="form-control">
+                @foreach ($empleats as $key => $empleat) 
+                  <option value="{{ $empleat->id_empleat }}">{{ $empleat->nom_empleat }} {{ $empleat->cognom1_empleat }} {{ $empleat->cognom2_empleat }}</option>
+                @endforeach
+              </select>
+            </div>
+
+          </div>
+        </form> {{-- end form --}}
+      </div>
+    </div>
+
+    {{-- PREPARACIÓ --}}
+    <div class="col-12 col-xl-6 mb-3">
+      <div class="card">
+        <form action=""> {{-- init form --}}
+          <div class="card-header">
+            <span class="h3">Preparació</span>
             <button class="btn btn-success float-right">
               <span class="fas fa-save"></span>Desar
             </button>
@@ -149,24 +246,60 @@
                 <option value="true">Sí</option>
               </select>
             </div>
+
+            <div class="form-group col-12 col-sm-6">
+              <label for="casting">Càsting</label>
+              <select name="casting" id="casting" class="form-control">
+                <option value="false" selected>No</option>
+                <option value="true">Sí</option>
+              </select>
+            </div>
+
+            <div class="form-group col-12 col-sm-6">
+              <label for="inserts">Inserts</label>
+              <select name="inserts" id="inserts" class="form-control">
+                <option value="No cal fer" selected>No cal fer</option>
+                <option value="Cal fer">Cal fer</option>
+                <option value="Fet">Fet</option>
+              </select>
+            </div>
+
+            <div class="form-group col-12 col-sm-6">
+              <label for="vec">VEC</label>
+              <select name="vec" id="vec" class="form-control">
+                <option value="false" selected>No</option>
+                <option value="true">Sí</option>
+              </select>
+            </div>
             
           </div>
         </form> {{-- end form --}}
       </div>
     </div>
 
-      {{-- EMPLEATS --}}
+    {{-- CONVOCATORIA --}}
     <div class="col-12 col-xl-6 mb-3">
       <div class="card">
         <form action=""> {{-- init form --}}
           <div class="card-header">
-            <span class="h3">Empleats</span>
+            <span class="h3">Convocatòria</span>
             <button class="btn btn-success float-right">
               <span class="fas fa-save"></span>Desar
             </button>
           </div>
           <div class="card-body row">
-            Formulario de empleados........
+            <div class="form-group col-12 col-sm-6">
+              <label for="convos">Convos</label>
+              <select name="convos" id="convos" class="form-control">
+                <option value="false" selected>No</option>
+                <option value="true">Sí</option>
+              </select>
+            </div>
+
+            <div class="form-group col-12 col-sm-6">
+              <label for="inici_sala">Inici sala</label>
+              <input type="date" name="inici_sala" id="inici_sala" class="form-control">
+            </div>
           </div>
         </form> {{-- end form --}}
       </div>
