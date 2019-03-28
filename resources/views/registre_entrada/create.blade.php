12a68f9bf3abd0f23d7793ffe99ed282ed873343
Denis <m8.denis.pereira@gmail.com>
2019-03-28 18:23:38 +0100

    Denis - StringToUpprt

--- resources/views/registre_entrada/create.blade.php
+++ resources/views/registre_entrada/create.blade.php
@@ -7,11 +7,11 @@
     <form method = "POST" action="{{ !empty($registreEntrada) ? route('registreEntradaUpdate', array('id' => $registreEntrada->id_registre_entrada)) : route('registreEntradaInsert') }}" enctype="multipart/form-data">
         @csrf
         <fieldset class="border p-2">
-            <legend class="w-auto">Dades:</legend>
+            <legend class="w-auto">DADES:</legend>
             <div class="row">
             <div class="col-6">
                     <div class="form-group">
-                        <label for="id_registre_entrada" style="font-weight: bold">Referencia:</label>
+                        <label for="id_registre_entrada" style="font-weight: bold">REFERENCIA:</label>
                         <input type="number" class="form-control" id="id_registre_entrada" placeholder="Entrar referencia" name="id_registre_entrada" value="{{!empty($registreEntrada) ? $registreEntrada->id_registre_entrada : old('id_registre_entrada')}}">
                         <span class="text-danger">{{ $errors->first('id_registre_entrada') }}</span>
                     </div>
@@ -20,14 +20,14 @@
             <div class="row">
                 <div class="col-6">
                     <div class="form-group">
-                        <label for="titol" style="font-weight: bold">Títol:</label>
+                        <label for="titol" style="font-weight: bold">TÍTOL:</label>
                         <input type="text" class="form-control" id="titol" placeholder="Entrar titol" name="titol" value="{{!empty($registreEntrada) ? $registreEntrada->titol : old('titol')}}">
                         <span class="text-danger">{{ $errors->first('titol') }}</span>
                     </div>
                 </div>
                 <div class="col-6">
                     <div class="form-group">
-                        <label for="client" style="font-weight: bold">Client:</label>
+                        <label for="client" style="font-weight: bold">CLIENT:</label>
                         <select class="form-control" name="id_client">
                             @foreach( $clients as $client )
                                 <option value="{{$client['id_client']}}" {{(!empty($registreEntrada) && $registreEntrada->id_client == $client['id_client']) || (old('id_client') == $client['id_client']) ? 'selected' : ''}} >{{$client['nom_client']}}</option>
@@ -56,14 +56,14 @@
             <div class="row">
                 <div class="col-6">
                     <div class="form-group">
-                        <label for="sortida" style="font-weight: bold">Data primera entrega:</label>
+                        <label for="sortida" style="font-weight: bold">DATA PRIMERA ENTREGA:</label>
                         <input type="date" class="form-control" id="sortida" placeholder="Entrar data Sortida" name="sortida" value="{{!empty($registreEntrada) ? explode(' ',$registreEntrada->sortida)[0] : ''}}">
                         <span class="text-danger">{{ $errors->first('sortida') }}</span>
                     </div>
                 </div>
                 <div class="col-6">
                     <div class="form-group">
-                        <label for="responsable" style="font-weight: bold">Responsable:</label>
+                        <label for="responsable" style="font-weight: bold">RESPONSABLE:</label>
                         <select class="form-control" name="id_usuari">
                             @foreach( $usuaris as $usuari )
                                 <option value="{{$usuari['id_usuari']}}" {{(!empty($registreEntrada) && $registreEntrada->id_usuari == $usuari['id_servei']) || (old('id_usuari') == $usuari['id_usuari']) ? 'selected' : ''}} >{{$usuari['nom_usuari'].' '.$usuari['cognom1_usuari'] }}</option>
@@ -77,7 +77,7 @@
             <div class="row">
                 <div class="col-6">
                     <div class="form-group">
-                        <label for="servei" style="font-weight: bold">Servei:</label>
+                        <label for="servei" style="font-weight: bold">SERVEI:</label>
                         <select class="form-control" name="id_servei">
                             @foreach( $serveis as $servei )
                                 <option value="{{$servei['id_servei']}}" {{(!empty($registreEntrada) && $registreEntrada->id_servei == $servei['id_servei']) || (old('id_servei') == $servei['id_servei']) ? 'selected' : ''}} >{{$servei['nom_servei']}}</option>
@@ -88,7 +88,7 @@
                 </div>
                 <div class="col-6">
                     <div class="form-group">
-                        <label for="idioma" style="font-weight: bold">Idioma:</label>
+                        <label for="idioma" style="font-weight: bold">IDIOMA:</label>
                         <select class="form-control" name="id_idioma">
                             @foreach( $idiomes as $idioma )
                                 <option value="{{$idioma['id_idioma']}}" {{(!empty($registreEntrada) && $registreEntrada->id_idioma == $idioma['id_idioma']) || (old('id_idioma') == $client['id_idioma']) ? 'selected' : ''}} >{{$idioma['idioma']}}</option>
@@ -103,7 +103,7 @@
             <div class="row">
                 <div class="col-6">
                     <div class="form-group">
-                        <label for="media" style="font-weight: bold">Tipus:</label>
+                        <label for="media" style="font-weight: bold">TIPUS:</label>
                         <select class="form-control" name="id_media" id="id_media">
                             @foreach( $medias as $media )
                                 <option value="{{$media['id_media']}}" {{(!empty($registreEntrada) && $registreEntrada->id_media == $media['id_media']) || (old('id_media') == $client['id_media']) ? 'selected' : ''}} >{{$media['nom_media']}}</option>
@@ -114,7 +114,7 @@
                 </div>
                 <div class="col-6">
                     <div class="form-group">
-                        <label for="minuts" style="font-weight: bold">Minuts:</label>
+                        <label for="minuts" style="font-weight: bold">MINUTS:</label>
                         <input type="number" class="form-control" id="minuts" name="minuts" value="{{!empty($registreEntrada) ? $registreEntrada->minuts : old('minuts')}}">
                         <span class="text-danger">{{ $errors->first('minuts') }}</span>
                     </div>
@@ -124,14 +124,14 @@
             <div class="row" id="total_ep">
                 <div class="col-6">
                     <div class="form-group">
-                        <label for="total_episodis" style="font-weight: bold">Episodis totals:</label>
+                        <label for="total_episodis" style="font-weight: bold">EPISODIS TOTALS:</label>
                         <input type="number" class="form-control" id="total_episodis" name="total_episodis" value="{{!empty($registreEntrada) ? $registreEntrada->total_episodis : old('total_episodis')}}">
                         <span class="text-danger">{{ $errors->first('total_episodis') }}</span>
                     </div>
                 </div>
                 <div class="col-6" id="ep_set">
                     <div class="form-group">
-                        <label for="episodis_setmanals" style="font-weight: bold">Episodis setmanals:</label>
+                        <label for="episodis_setmanals" style="font-weight: bold">EPISODIS SETMANALS:</label>
                         <input type="number" class="form-control" id="episodis_setmanals" name="episodis_setmanals" value="{{!empty($registreEntrada) ? $registreEntrada->episodis_setmanals : old('episodis_setmanals')}}">                    
                         <span class="text-danger">{{ $errors->first('episodis_setmanals') }}</span>
                     </div>
@@ -140,14 +140,14 @@
             <div class="row">
                 <div class="col-6" id="ent_set">
                     <div class="form-group">
-                        <label for="entregues_setmanals" style="font-weight: bold">Entregues setmanals:</label>
+                        <label for="entregues_setmanals" style="font-weight: bold">ENTREGUES SETMANALS:</label>
                         <input type="number" class="form-control" id="entregues_setmanals" name="entregues_setmanals" value="{{!empty($registreEntrada) ? $registreEntrada->entregues_setmanals : old('entregues_setmanals')}}">
                         <span class="text-danger">{{ $errors->first('entregues_setmanals') }}</span>
                     </div>
                 </div>
                 <div class="col-6">
                     <div class="form-group">
-                        <label for="estat" style="font-weight: bold">Estat:</label>
+                        <label for="estat" style="font-weight: bold">ESTAT:</label>
                         <select class="form-control" name="estat">
                             <option value="Pendent" {{(!empty($registreEntrada) && $registreEntrada->estat == 'Pendent') || (old('estat') == 'Pendent') ? 'selected' : ''}}>Pendent</option>
                             <option value="Finalitzada" {{(!empty($registreEntrada) && $registreEntrada->estat == 'Finalitzada') || (old('estat') == 'Finalitzada')  ? 'selected' : ''}}>Finalitzada</option>
@@ -166,10 +166,10 @@
         <div class="row justify-content-between">
             <a href="{{ url('/registreEntrada') }}" class="btn btn-primary col-2">
                 <span class="fas fa-angle-double-left"></span>
-                Tornar enrera
+                TORNAR ENRERA
             </a>
 
-            <button type="submit" class="btn btn-success col-2">{{!empty($registreEntrada) ? 'Desar canvis' : 'Crear'}} <i class="fas fa-save"></i></button>
+            <button type="submit" class="btn btn-success col-2">{{!empty($registreEntrada) ? 'DESAR CANVIS' : 'CREAR'}} <i class="fas fa-save"></i></button>
 
         </div>
         <br>
