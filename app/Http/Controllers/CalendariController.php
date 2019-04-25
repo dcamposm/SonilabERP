38efc46b354e6dd2a389acd6452421d363ba63fa
David Ortiz <cmortizdavid@gmail.com>
2019-04-25 19:40:00 +0200

    David - Abraham - Pau: Migraciones calendario y calendario carrec

--- app/Http/Controllers/CalendariController.php
+++ app/Http/Controllers/CalendariController.php
@@ -4,10 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Validator;
 
 class CalendariController extends Controller
 {
     public function showCalendari(){
         return View('calendari.index');
+    }
+    public function create(){
+        $v = Validator::make(request()->all(),[
+            'id_calendar'=>'required|max:35',
+            'id_empleat'=>'required|max:35',
+            'id_registre_entrada'=>'required|max:35',
+            'num_takes'=>'required|regex:/[]/',
+            'data_inici'=>'required|max:35',
+            'data_fi'=>'required|max:35',
+            'num_sala'=>'required|max:35'
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -151,4 +151,6 @@ Route::middleware(['role: 1,2,4'])->group(function () {
 //------------------Rutes calendari------------------
 Route::middleware(['role: 1,2,3,4'])->group(function () {
     Route::get('/calendari', 'CalendariController@showCalendari')->name('showCalendari');
+    Route::get('/calendari/crear', 'CalendariController@createCalendari')->name('createCalendari');
+    Route::post('/calendari/esborrar', 'CalendariController@deleteCalendari')->name('deleteCalendari');
 });
