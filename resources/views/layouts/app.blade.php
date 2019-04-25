225ba9e8bc0138c48f5bf1ff3bd904bb4b6e0b34
Denis <m8.denis.pereira@gmail.com>
2019-04-25 17:45:54 +0200

    Denis - Cambio de logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -77,7 +77,7 @@
 
     <nav class="navbar navbar-dark fixed-top bg-dark navbar-sonilab flex-md-nowrap p-0 justify-content-between">
 
-        <a class="navbar-brand ml-2" href="{{ route('home') }}"><img src="http://sonilab.com.mialias.net/wp-content/uploads/2016/11/SONILAB_logo41-300x65-300x75.png" width="120" height="30" class="d-inline-block align-top" alt=""></a>
+        <a class="navbar-brand ml-2" href="{{ route('home') }}"><img src="{{ asset('img/logo_letras.png') }}" height="30" class="d-inline-block align-top" alt=""></a>
         @auth
 
         <ul class="navbar-nav px-3">
