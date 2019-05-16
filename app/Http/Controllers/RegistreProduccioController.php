35c3efde3a529f9dfabd38e44737f800dcb17927
dcampos <dcampos@A0001.SLAB.local>
2019-05-16 12:37:14 +0200

    Demetrio - Acabat de implementar Classes Responsables en el controlador RegistreProduccio.

--- app/Http/Controllers/RegistreProduccioController.php
+++ app/Http/Controllers/RegistreProduccioController.php
@@ -13,6 +13,7 @@ use Auth;
 use Validator;
 use App\Http\Responsables\RegistreProduccio\RegistreProduccioIndex;
 use App\Http\Responsables\RegistreProduccio\RegistreProduccioCreate;
+use App\Http\Responsables\RegistreProduccio\RegistreProduccioShow;
 
 class RegistreProduccioController extends Controller {
 
@@ -31,13 +32,9 @@ class RegistreProduccioController extends Controller {
     }
 
     public function show($id) {
-        $empleatsCarrec = EmpleatExtern::with('carrec')->get();
-        //return response()->json(Auth::user()->id_usuari);
-        // Solamente tenemos que cargar los registros de entrada pendientes.
-        $regEntrades = RegistreEntrada::whereEstat('Pendent')->get();
         $registreProduccio = RegistreProduccio::with('registreEntrada')->find($id);
         
-        //----------Si entra el reponsable del registre d'entrada i el registre te un missatge NEW, elimina el missatge---------------
+//----------Si entra el reponsable del registre d'entrada i el registre te un missatge NEW, elimina el missatge---------------
         if ($registreProduccio->registreEntrada->id_usuari == Auth::user()->id_usuari){
             Missatge::where([['id_referencia', $id],
                             ['missatge', 'NEW'],
@@ -45,26 +42,7 @@ class RegistreProduccioController extends Controller {
                             ->delete();
         }
         
-        //return response()->json($registreProduccio);
-        $empleados   = [];
-        $traductor   = EmpleatExtern::find($registreProduccio["id_traductor"]);
-        $ajustador   = EmpleatExtern::find($registreProduccio->id_ajustador);
-        $linguista   = EmpleatExtern::find($registreProduccio->id_linguista);
-        $director    = EmpleatExtern::find($registreProduccio->id_director);
-        $tecnic_mix  = EmpleatExtern::find($registreProduccio->id_tecnic_mix);
-
-        if ($traductor) $empleados["traductor"] = $traductor;
-        if ($ajustador) $empleados["ajustador"] = $ajustador;
-        if ($linguista) $empleados["linguista"] = $linguista;
-        if ($director) $empleados["director"] = $director;
-        if ($tecnic_mix) $empleados["tecnic_mix"] = $tecnic_mix;
-
-        return view('registre_produccio.show', array(
-            'registreProduccio' => $registreProduccio,
-            'empleats'          => $empleados,
-            'empleatsCarrec' => $empleatsCarrec,
-            'regEntrades' => $regEntrades
-        ));
+        return new RegistreProduccioShow($registreProduccio);
     }
 
     public function updateView($id){
@@ -84,10 +62,8 @@ class RegistreProduccioController extends Controller {
                 if (!$registre){
                     return redirect()->back()->withErrors(array('error' => 'ERROR. No s\'ha pogut modificar. No es pot repetir subreferencies.'));
                 }
-                //return response()->json($registre);
             }
         }
-        //return response()->json(request()->all());
         $prod->fill(request()->all());               
 
         try {
@@ -116,7 +92,6 @@ class RegistreProduccioController extends Controller {
 
         if ($v->fails()) {
             return redirect()->back()->withErrors($v)->withInput();
-            //return redirect()->back()->withErrors(array('error' => 'ERROR. No s\'han introduit totes les dades'));
         } else {
             $prod->fill(request()->all());               
 
@@ -146,7 +121,6 @@ class RegistreProduccioController extends Controller {
 
         if ($v->fails()) {
             return redirect()->back()->withErrors($v)->withInput();
-            //return redirect()->back()->withErrors(array('error' => 'ERROR. No s\'han introduit totes les dades'));
         } else {
             $prod->fill(request()->all());               
 
@@ -178,7 +152,6 @@ class RegistreProduccioController extends Controller {
 
         if ($v->fails()) {
             return redirect()->back()->withErrors($v)->withInput();
-            //return redirect()->back()->withErrors(array('error' => 'ERROR. No s\'han introduit totes les dades'));
         } else {
             $prod->fill(request()->all());               
 
@@ -195,7 +168,6 @@ class RegistreProduccioController extends Controller {
     public function updateConvocatoria($id){
 
         $prod = RegistreProduccio::find($id);
-        //return response()->json($prod);
         $v = Validator::make(request()->all(), [
             'convos'            => 'required',
             'inici_sala'        => 'date',
@@ -208,7 +180,6 @@ class RegistreProduccioController extends Controller {
 
         if ($v->fails()) {
             return redirect()->back()->withErrors($v)->withInput();
-            //return redirect()->back()->withErrors(array('error' => 'ERROR. No s\'han introduit totes les dades'));
         } else {
             $prod->fill(request()->all());               
 
@@ -223,11 +194,10 @@ class RegistreProduccioController extends Controller {
     }
 
     public function find() {
-        //return response()->json(request()->all());
         if (request()->input("searchBy") == 'id_traductor' || request()->input("searchBy") == 'id_ajustador' || 
             request()->input("searchBy") == 'id_linguista' || request()->input("searchBy") == 'id_director'  || 
             request()->input("searchBy") == 'id_tecnic_mix'){
-            //->whereRaw('LOWER(nom_empleat) like "%'. strtolower(request()->input("search_term")).'%"')
+            
             if (request()->input("searchBy") == 'id_traductor'){
                 $empleats = EmpleatExtern::with(['carrec' => function($query){
                                 $query->where('id_tarifa', 12);
@@ -249,27 +219,24 @@ class RegistreProduccioController extends Controller {
                                 $query->where('id_carrec', 3);
                             }])->whereRaw('LOWER(nom_empleat) like "%'. strtolower(request()->input("search_term")).'%"')->get();
             }
-            //return response()->json($empleats);
+
             foreach ($empleats as $empleat){
-                //return response()->json(empty($empleat->carrec[0]));
+
                 if (!empty($empleat->carrec[0])){
-                    //return response()->json($empleat);
                     if (!isset($raw)){
                         $raw = request()->input("searchBy").' = '.$empleat->id_empleat.'';
                     } else {
                         $raw = $raw.' OR '.request()->input("searchBy").' = '.$empleat->id_empleat.'';
                     }
-                    //return response()->json($empleat);
                 }
             }
             if (!isset($raw)){
                 $raw = request()->input("searchBy").' like "'.strtolower(request()->input("search_term")).'"';;
             }
-            //return response()->json($raw);
+
             $registres = RegistreProduccio::with('traductor')->with('ajustador')
                             ->with('linguista')->with('director')->with('tecnic')->with('getEstadillo')
                             ->orderBy('estat')->orderBy('data_entrega')->orderBy(request()->input("orderBy"))->whereRaw($raw)->get();
-            //return response()->json($registres);
         } else {
             $registres = RegistreProduccio::with('traductor')->with('ajustador')
                 ->with('linguista')->with('director')->with('tecnic')->with('getEstadillo')
@@ -322,7 +289,6 @@ class RegistreProduccioController extends Controller {
 
         if ($v->fails()) {
             return redirect()->back()->withErrors($v)->withInput();
-            //return redirect()->back()->withErrors(array('error' => 'ERROR. No s\'han introduit totes les dades'));
         } else {
             $prod = new RegistreProduccio(request()->all());               
 
@@ -351,7 +317,6 @@ class RegistreProduccioController extends Controller {
 
         if ($v->fails()) {
             return redirect()->back()->withErrors($v)->withInput();
-            //return redirect()->back()->withErrors(array('error' => 'ERROR. No s\'han introduit totes les dades'));
         } else {
             $prod = new RegistreProduccio(request()->all());               
 
@@ -378,7 +343,6 @@ class RegistreProduccioController extends Controller {
 
         if ($v->fails()) {
             return redirect()->back()->withErrors($v)->withInput();
-            //return redirect()->back()->withErrors(array('error' => 'ERROR. No s\'han introduit totes les dades'));
         } else {
             $prod = new RegistreProduccio(request()->all());               
 
