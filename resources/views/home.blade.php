00997ada891ceca30559c10eb50c4d3ffd4b0461
dcampos <dcampos@A0001.SLAB.local>
2019-06-05 12:18:27 +0200

    Demetrio - Millores vista home.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -58,7 +58,7 @@
                     @if (Auth::user()->hasRole('4'))
                        @foreach ($user as $usuari)
                             @foreach($usuari->missatgeDay->where('type', 'registreEntradaUpdate') as $missatge)
-                                <div class="alert alert-success" role="alert"> <a class="alert-link" href="{{route('mostrarRegistreEntrada', array('id' => $missatge->id_referencia))}}">{{$missatge->missatge}}</a></div>
+                                <div class="alert alert-success" role="alert"> <a class="alert-link" href="{{route('mostrarRegistreEntrada', array('id' => $missatge->id_referencia))}}">{{$missatge->missatge}} - {{$usuari->alias_usuari}}</a></div>
                             @endforeach
                        @endforeach
                     @else
@@ -79,12 +79,40 @@
                     @if (Auth::user()->hasRole('4'))
                        @foreach ($user as $usuari)
                             @foreach($usuari->missatgeDay->where('type', 'alertEntrega') as $missatge)
-                                <div class="alert alert-warning" role="alert"> <a class="alert-link" href="{{route('mostrarRegistreEntrada', array('id' => $missatge->id_referencia))}}">{{$missatge->missatge}}</a></div>
+                                @if ($missatge->referencia == 'registreEntrada'  && date("Y-m-d",strtotime(date("d-m-Y")."+ 7 days")) >= $missatge->data_final)
+                                    <div class="alert alert-warning" role="alert">
+                                        <form method = "GET" action= '{{ route('registreProduccioFind') }}' id='search'>
+                                            @csrf
+                                            <input type="hidden" id="searchBy" class="form-control" name="searchBy" value="id_registre_entrada">
+                                            <input type="hidden" id="orderBy" class="form-control" name="orderBy" value="id_registre_entrada">
+                                            <input type="hidden" id="search_term" class="form-control" name="search_term" value="{{$missatge->id_referencia}}">
+                                            <a class="alert-link" onclick="this.parentNode.submit();" href="#">
+                                                {{$missatge->missatge}} - {{$usuari->alias_usuari}}
+                                            </a>
+                                        </form>
+                                    </div>
+                                @elseif ($missatge->referencia == 'registreProduccio' && date("Y-m-d",strtotime(date("d-m-Y")."+ 7 days")) >= $missatge->data_final)
+                                    <div class="alert alert-warning" role="alert"> <a class="alert-link" href="{{route('mostrarRegistreProduccio', array('id' => $missatge->id_referencia))}}">{{$missatge->missatge}}</a></div>
+                                @endif
                             @endforeach
                        @endforeach
                     @else
                         @forelse($user->missatgeDay->where('type', 'alertEntrega') as $missatge)
-                            <div class="alert alert-warning" role="alert"> <a class="alert-link" href="{{route('mostrarRegistreEntrada', array('id' => $missatge->id_referencia))}}">{{$missatge->missatge}}</a></div>
+                            @if ($missatge->referencia == 'registreEntrada' && date("Y-m-d",strtotime(date("d-m-Y")."+ 7 days")) >= $missatge->data_final)
+                                <div class="alert alert-warning" role="alert">
+                                    <form method = "GET" action= '{{ route('registreProduccioFind') }}' id='search'>
+                                        @csrf
+                                        <input type="hidden" id="searchBy" class="form-control" name="searchBy" value="id_registre_entrada">
+                                        <input type="hidden" id="searchBy" class="form-control" name="orderBy" value="id_registre_entrada">
+                                        <input type="hidden" id="search_term" class="form-control" name="search_term" value="{{$missatge->id_referencia}}">
+                                        <a class="alert-link" onclick="this.parentNode.submit();" href="#">
+                                            {{$missatge->missatge}}
+                                        </a>
+                                    </form>
+                                </div>
+                            @elseif ($missatge->referencia == 'registreProduccio' && date("Y-m-d",strtotime(date("d-m-Y")."+ 7 days")) >= $missatge->data_final)
+                                <div class="alert alert-warning" role="alert"> <a class="alert-link" href="{{route('mostrarRegistreProduccio', array('id' => $missatge->id_referencia))}}">{{$missatge->missatge}}</a></div>
+                            @endif
                         @empty
                             No tens missatges.
                         @endforelse
